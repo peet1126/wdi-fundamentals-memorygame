f584ce8b431afd7be5da02a958c2f5599bf45b07
Emily  Robertson <emilyrobertson@Emilys-MBP-2.lan1>
2019-04-13 15:33:27 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,20 @@
-console.log("Up and Running!");
-var cardOne = 'king';
-var cardTwo = 'king';
-var cardThree = 'queen';
-var cardFour = 'queen';
+var cards = ["qween", "qween", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
 console.log("User flipped " + cardTwo);
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] == cardsInPlay[1]) {
+		alert("you found a match!");
+	} else {
+		alert("sorry, try again.");
+	}
+}
+
+
+
+
+
